ee1233fcace0f0b8666c90cfe6f6a0404a5e8ac2
namidapoo <83203852+ve1997@users.noreply.github.com>
2025-02-22 19:16:03 +0900

    feat: アクティブリンクのスタイル設定

--- app/root.tsx
+++ app/root.tsx
@@ -2,9 +2,9 @@ import type { LinksFunction } from "@remix-run/node";
 import { json, redirect } from "@remix-run/node";
 import {
 	Form,
-	Link,
 	Links,
 	Meta,
+	NavLink,
 	Outlet,
 	Scripts,
 	ScrollRestoration,
@@ -61,7 +61,12 @@ export default function App() {
 							<ul>
 								{contacts.map((contact) => (
 									<li key={contact.id}>
-										<Link to={`contacts/${contact.id}`}>
+										<NavLink
+											className={({ isActive, isPending }) =>
+												isActive ? "active" : isPending ? "pending" : ""
+											}
+											to={`contacts/${contact.id}`}
+										>
 											{contact.first || contact.last ? (
 												<>
 													{contact.first} {contact.last}
@@ -70,7 +75,7 @@ export default function App() {
 												<i>名前なし</i>
 											)}{" "}
 											{contact.favorite ? <span>★</span> : null}
-										</Link>
+										</NavLink>
 									</li>
 								))}
 							</ul>

8ac84e2c8bc0abe8d424692d213d4a554ec904cc
namidapoo <83203852+ve1997@users.noreply.github.com>
2025-02-22 12:13:43 +0900

    feat: パラメータの検証とレスポンスの送出

--- app/routes/contacts.$contactId.tsx
+++ app/routes/contacts.$contactId.tsx
@@ -1,13 +1,17 @@
-import { json } from "@remix-run/node";
+import { type LoaderFunctionArgs, json } from "@remix-run/node";
 import { Form, useLoaderData } from "@remix-run/react";
 import type { FunctionComponent } from "react";
+import invariant from "tiny-invariant";
+import { type ContactRecord, getContact } from "../data";
 
-import { getContact, type ContactRecord } from "../data";
-
-export const loader = async ({ params }) => {
-    const contact = await getContact(params.contactId);
-    return json({ contact });
-  };
+export const loader = async ({ params }: LoaderFunctionArgs) => {
+	invariant(params.contactId, "Missing contactId param");
+	const contact = await getContact(params.contactId);
+	if (!contact) {
+		throw new Response("Not Found", { status: 404 });
+	}
+	return json({ contact });
+};
 
 export default function Contact() {
 	const { contact } = useLoaderData<typeof loader>();
